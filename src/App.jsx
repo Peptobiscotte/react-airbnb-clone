8bc00c78596e2da7bc495aaee6145899e03cb9de
Pepto <monsieurfranci@gmail.com>
2023-10-20 18:39:53 +0200

    add card & more app render

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import data from "./data"
+import Card from "./components/Card"
+
 
 function App() {
+  const cards = data.map(item => {
+    return (
+        <Card
+            key={item.id}
+            {...item}
+            
+        />
+    )
+})  
+
   return (
     <div>
       <Navbar />
       <Hero />
+      <section className="cards-list">
+        {cards}
+      </section>
     </div>
   )
 }
