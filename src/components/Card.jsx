a36bfac22189f874aa1deb984e29852e29c05f84
Pepto <monsieurfranci@gmail.com>
2023-10-23 15:07:20 +0200

    add Card section and style

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,5 @@
-import katie from "../assets/katie-zaferes.png"
-import bike from "../assets/mountain-bike.png"
-import wedding from "../assets/wedding-photography.png"
+/* eslint-disable react/prop-types */
+
 
 export default function Card(props) {
     let badgeText
@@ -17,11 +16,11 @@ export default function Card(props) {
                 <div className="card--badge">{badgeText}</div>
             }
             <img 
-                src={`../images/${props.coverImg}`} 
+                src={`../assets/${props.coverImg}`} 
                 className="card--image" 
             />
             <div className="card--stats">
-                <img src="../images/star.png" className="card--star" />
+                <img src="/assets/star.png" className="card--star" />
                 <span>{props.stats.rating}</span>
                 <span className="gray">({props.stats.reviewCount}) • </span>
                 <span className="gray">{props.location}</span>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
-import airBnbLogo from "../assets/airbnb-logo.png"
+
 
 export default function Navbar() {
     return (
         <nav>
-            <img src={airBnbLogo} className="nav--logo" />
+            <img src="/assets/airbnb-logo.png" className="nav--logo" />
         </nav>
     )
 }
